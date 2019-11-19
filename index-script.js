5809692a76197d94759211086d120bdd3dc469ca
Ishan Goel <ishan@Macintosh.local>
2019-11-19 13:03:09 +0400

    added download button

--- index-script.js
+++ index-script.js
@@ -86,3 +86,17 @@ function email() {
   console.log(content);
   window.open('mailto:test@example.com?subject=Your Notes From quackduck.github.io&body=' + content);
 }
+
+function download(){
+    var text = document.getElementById("mytextarea").value;
+    text = text.replace(/\n/g, "\r\n"); // To retain the Line breaks.
+    var blob = new Blob([text], { type: "text/plain"});
+    var anchor = document.createElement("a");
+    anchor.download = "notes-from-quackduck.github.io.txt";
+    anchor.href = window.URL.createObjectURL(blob);
+    anchor.target ="_blank";
+    anchor.style.display = "none"; // just to be safe!
+    document.body.appendChild(anchor);
+    anchor.click();
+    document.body.removeChild(anchor);
+ }
