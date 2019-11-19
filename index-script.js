fd8c1c3e229532f67f2edf778eee910c518266f7
Ishan Goel <ishan@Macintosh.local>
2019-11-19 11:55:51 +0400

    <brag style="happy">Added email your self your notes functionality</brag>

--- index-script.js
+++ index-script.js
@@ -71,17 +71,18 @@ $.ajax({
 
 
 function offline() {
-  alert ("You Are Offline, Some Features Might Not Work");
+  alert ("You are offline, some features might not work");
 }
 function online() {
   alert ("You are back online");
 }
-function myprint() {
-  window.print();
-}
 
-function showContent() {
-  var temp = document.getElementsByTagName("template")[0];
-  var clon = temp.content.cloneNode(true);
-  document.body.appendChild(clon);
+
+
+
+function email() {
+
+  var content = document.getElementById("mytextarea").value;
+  console.log(content);
+  window.open('mailto:test@example.com?subject=Your Notes From quackduck.github.iot&body=' + content);
 }
