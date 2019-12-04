1b2075c6f2491bf2cc48598d98d8102ab0b37725
Ishan Goel <goelishan2005@gmail.com>
2019-12-04 06:21:47 +0400

    added instant result to electric

--- electric/script.js
+++ electric/script.js
@@ -9,7 +9,7 @@ function submit() {
   var years = parseFloat(document.getElementById("years").value);
 
 
-  var electperyear = electcost*(distance/315)*55.5
+  var electperyear = electcost*(distance/350)*55.5
   console.log("electperyear=");
   console.log(electperyear);
 
@@ -27,32 +27,63 @@ function submit() {
   console.log(gascartotalcost);
 
   if (electcartotalcost<gascartotalcost) {
-    alert("TESLA WINS")
+    alert("TESLA WINS");
   }
   else if (electcartotalcost==gascartotalcost) {
-    alert("uhhh this is awkward. the cost is the same")
+    alert("uhhh this is awkward. the cost is the same");
   }
 
   else {
-    alert("GAS CAR WINS and goes on to destroy earth")
+    alert("GAS CAR WINS and goes on to destroy earth");
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
+
+  function newsubmit() {
+    console.log("clicked");
+    var mileage = parseFloat(document.getElementById("mileage").value);
+    var gascost = parseFloat(document.getElementById("gascost").value);
+    var carcost = parseFloat(document.getElementById("carcost").value);
+    var maintcost = parseFloat(document.getElementById("maintcost").value);
+    var electcost = parseFloat(document.getElementById("electcost").value);
+    var distance = parseFloat(document.getElementById("distance").value);
+    var years = parseFloat(document.getElementById("years").value);
+
+
+    var electperyear = electcost*(distance/350)*55.5
+    console.log("electperyear=");
+    console.log(electperyear);
+
+    var electcartotalcost = 128450+(electperyear*years)
+    console.log("electcartotalcost=");
+    console.log(electcartotalcost);
+
+
+    var gasperyear = maintcost+(gascost*(distance/mileage))
+    console.log("gasperyear=");
+    console.log(gasperyear);
+
+    var gascartotalcost = carcost+(gasperyear*years)
+    console.log("gascartotalcost=");
+    console.log(gascartotalcost);
+
+    //var equalyears = mileage*(128450-carcost)/(maintcost*mileage+(gascost*distance)-(0.43104183*mileage))
+
+    if (electcartotalcost<gascartotalcost) {
+      document.getElementById("result").innerHTML= "TESLA WINS";
+    }
+    else if (electcartotalcost==gascartotalcost) {
+      document.getElementById("result").innerHTML= "uhhh this is awkward. the cost is the same" ;
+    }
+
+    else {
+      document.getElementById("result").innerHTML= "GAS CAR WINS and goes on to destroy earth"//<br> At " + equalyears + " years the total cost of this gas car and the Tesla is the same";
+    }
+
+
+    if (isNaN(mileage)||isNaN(gascost)||isNaN(carcost)||isNaN(maintcost)||isNaN(electcost)||isNaN(distance)||isNaN(years)) {
+      document.getElementById("result").innerHTML= "Fill in the values and the result will be shown here";
+    }
+
+
+
+  }
