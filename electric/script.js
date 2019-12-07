02a267c6f7a181803dd07afb1f5aa8400f5fbe53
Ishan Goel <goelishan2005@gmail.com>
2019-12-07 13:45:33 +0400

    Made electric better looking

--- electric/script.js
+++ electric/script.js
@@ -1,42 +1,7 @@
-function submit() {
-  console.log("clicked");
-  var mileage = parseFloat(document.getElementById("mileage").value);
-  var gascost = parseFloat(document.getElementById("gascost").value);
-  var carcost = parseFloat(document.getElementById("carcost").value);
-  var maintcost = parseFloat(document.getElementById("maintcost").value);
-  var electcost = parseFloat(document.getElementById("electcost").value);
-  var distance = parseFloat(document.getElementById("distance").value);
-  var years = parseFloat(document.getElementById("years").value);
+window.addEventListener("DOMContentLoaded", function() {
+  newsubmit();
 
-
-  var electperyear = electcost*(distance/350)*55.5
-  console.log("electperyear=");
-  console.log(electperyear);
-
-  var electcartotalcost = 128450+(electperyear*years)
-  console.log("electcartotalcost=");
-  console.log(electcartotalcost);
-
-
-  var gasperyear = maintcost+(gascost*(distance/mileage))
-  console.log("gasperyear=");
-  console.log(gasperyear);
-
-  var gascartotalcost = carcost+(gasperyear*years)
-  console.log("gascartotalcost=");
-  console.log(gascartotalcost);
-
-  if (electcartotalcost<gascartotalcost) {
-    alert("TESLA WINS");
-  }
-  else if (electcartotalcost==gascartotalcost) {
-    alert("uhhh this is awkward. the cost is the same");
-  }
-
-  else {
-    alert("GAS CAR WINS and goes on to destroy earth");
-  }
-}
+});
 
   function newsubmit() {
     console.log("clicked");
