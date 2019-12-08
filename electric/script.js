19801f243773702a30ca3aeeabd5c656bf6b47ea
Ishan Goel <goelishan2005@gmail.com>
2019-12-08 11:12:22 +0400

    improved truncating and deleted .DS_Store

--- electric/script.js
+++ electric/script.js
@@ -14,13 +14,23 @@ function submit() {
   var gasperyear = maintcost + (gascost * (distance / mileage))
   var gascartotalcost = carcost + (gasperyear * years)
   if (electcartotalcost < gascartotalcost) {
-    document.getElementById("result").innerHTML = "TESLA WINS <br> It won by " + (Math.trunc(gascartotalcost - electcartotalcost)) + " AED";
+    document.getElementById("result").innerHTML = "TESLA WINS <br> It won by " + (truncate(gascartotalcost - electcartotalcost)) + " AED";
   } else if (electcartotalcost == gascartotalcost) {
     document.getElementById("result").innerHTML = "uhhh this is awkward. the cost is the same";
   } else {
-    document.getElementById("result").innerHTML = "GAS CAR WINS and goes on to destroy earth <br> It won by " + (Math.trunc(electcartotalcost - gascartotalcost)) + " AED" //<br> At " + equalyears + " years the total cost of this gas car and the Tesla is the same";
+    document.getElementById("result").innerHTML = "GAS CAR WINS and goes on to destroy earth <br> It won by " + (truncate(electcartotalcost - gascartotalcost)) + " AED" //<br> At " + equalyears + " years the total cost of this gas car and the Tesla is the same";
   }
   if (isNaN(mileage) || isNaN(gascost) || isNaN(carcost) || isNaN(maintcost) || isNaN(electcost) || isNaN(distance) || isNaN(years)) {
     document.getElementById("result").innerHTML = "Fill in the values and the result will be shown here";
   }
+}
+
+function truncate(val) {
+  if (val<1) {
+    val = (Math.trunc(100*val)/100)
+  }
+  else {
+    val = Math.trunc(val);
+  }
+  return val;
 }
