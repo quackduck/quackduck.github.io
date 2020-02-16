1f8781a7a4ab21b12981c481a4f2139534e806a4
Ishan Goel <goelishan2005@gmail.com>
2020-02-16 19:47:18 +0400

    stuff

--- knowledgehub/glossary/script.js
+++ knowledgehub/glossary/script.js
@@ -3,29 +3,67 @@ window.addEventListener("DOMContentLoaded", function() {
 
 });
 
-
-
-
 function makeclouds() {
   for (var i = 1; i <= 64; i++) {
     var canvas = document.createElement("CANVAS"); // Create a <button> element
     canvas.setAttribute("id", "cloud" + i);
     canvas.setAttribute("height", 200);
     canvas.setAttribute("width", 578);
-    document.body.appendChild(canvas);
+    document.getElementById("clouds").appendChild(canvas);
   }
-  makecloud("HEEHEE", "blehh", 1);
-  makecloud("check", "lord gryffin", 2);
-  makecloud("checfk", "lord gryffddewin", 3);
+  makecloud("Biodiversity", "Number and types of species in the wild", 1);
+  makecloud("Biodiversity Hotspots", "A habitat with a high number of different species", 2);
+  makecloud("Bioindicators", "Species used as a measure of some aspects of an ecosystem's health", 3);
+  makecloud("Blue Carbon Sinks", "Coastal ecosystems which store more carbon than land forests", 4);
+  makecloud("Camouflage", "Hiding by disguising", 5);
+  makecloud("Climate Change", "Human caused long term changes to global climates", 6);
+  makecloud("Conservation", "Restore, protect and preserve ecosystems", 7);
+  makecloud("Coral Reefs", "An ecosystem of reef building corals", 8);
+  makecloud("Denning", "A habitat or retreat of an animal", 9);
+  makecloud("Ecosystem", " A combination of Non living things and Living things interacting ", 10);
+  makecloud("Ecosystem Services", "How humans benefit from a healthy ecosystem", 11);
+  makecloud("Endemic Species", "A native species not found anywhere else", 12);
+  makecloud("Environmental Impact", "Impact on the environment as a result of something", 13);
+  makecloud("Extinction", "What happens to a species when all of its members die out", 14);
+  makecloud("Fauna", "All life present in a particular region, habitat or time", 15);
+  makecloud("Flagship Species", "A symbolic species of a habitat", 16);
+  makecloud("Flora", "All plants in a region, habitat or time", 17);
+  makecloud("", "", 18);
+  makecloud("", "", 19);
+  makecloud("", "", 20);
+  makecloud("", "", 21);
+  makecloud("", "", 23);
+  makecloud("", "", 24);
+  makecloud("", "", 25);
+  makecloud("", "", 26);
+  makecloud("", "", 27);
+  makecloud("", "", 28);
+  makecloud("", "", 29);
+  makecloud("", "", 30);
+  makecloud("", "", 31);
+  makecloud("", "", 32);
+  makecloud("", "", 33);
+  makecloud("", "", 34);
+  makecloud("", "", 35);
+  makecloud("", "", 36);
+  makecloud("", "", 37);
+  makecloud("", "", 38);
+  makecloud("", "", 39);
+  makecloud("", "", 40);
+  makecloud("", "", 41);
+  makecloud("", "", 42);
+  makecloud("", "", 43);
+  makecloud("", "", 44);
+  makecloud("", "", 45);
+  makecloud("", "", 46);
+  makecloud("", "", 47);
+  makecloud("", "", 48);
+  makecloud("", "", 49);
+  makecloud("", "", 50);
+
+
+
 }
-
-
-
-
-
-
-
-
 
 function makecloud(word, meaning, id) {
   // draw cloud
@@ -47,12 +85,12 @@ function makecloud(word, meaning, id) {
   context.fill();
   context.strokeStyle = 'lightblue';
   context.stroke();
-  context.font = "1.5em Futura";
+  context.font = "1.3em Futura";
   context.fillStyle = "black";
   context.textAlign = "center";
   context.fillText(word, canvas.width / 2, canvas.height / 2.5);
   canvas.onclick = function() {
-    context.font = "1em Futura";
+    context.font = "0.8em Futura";
     context.fillStyle = "black";
     context.textAlign = "center";
     context.fillText(meaning, canvas.width / 2, canvas.height / 1.7);
